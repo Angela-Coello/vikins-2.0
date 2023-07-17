1c2db18b9e3eb3f996bb57f9c5576bb57b5e0df9
Angela-coello <angeladarko32@gmail.com>
2023-07-17 11:50:06 +0200

    subir archibos

---
+++ OneDrive/Escritorio/git-repos/vikins-2.0/src/main/java/org/example/vikins/Vikings.java
@@ -0,0 +1,39 @@
+package org.example.vikins;
+
+import org.example.soldier.Soldier;
+
+class Viking extends Soldier {
+ public String name;
+ public  String msg;
+    public Viking(String name, int health, int strength) {
+        super(health, strength);
+        this.name = name;
+    }
+
+
+
+
+
+    public String receiveDamage(int damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            this.msg =this.name + "has received" + damage + "points of damage";
+
+        } else {
+            this.msg =this.name + "has died in combat";
+
+        }
+        return String.valueOf(this.health);
+    }
+    public String getMsg(){
+        return this.msg;
+    }
+    String battleCry(){
+        return "Odin Owns You All!";
+    }
+}
+
+
+
+
+
---
+++ OneDrive/Escritorio/git-repos/vikins-2.0/src/test/java/org/example/soldier/SoldierTest.java
@@ -0,0 +1,51 @@
+package org.example.soldier;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SoldierTest {
+    int strength = 150;
+    int health = 300;
+
+    @Test
+    public void should_receive_2_arguments() {
+        Soldier soldier = new Soldier(health, strength);
+        assertNotNull(soldier);
+    }
+
+    @Test
+    public void should_receive_the_health_property_as_its_1st_argument() {
+        Soldier soldier = new Soldier(health, strength);
+        assertEquals(300, soldier.health);
+    }
+
+    @Test
+    public void should_receive_the_health_property_as_its_2st_argument() {
+        Soldier soldier = new Soldier(health, strength);
+        assertEquals(150, soldier.strength);
+    }
+
+    @Test
+    public void attack_should_receive_0_arguments() {
+        Soldier soldier = new Soldier(health, strength);
+        int damage = 50;
+        int resultAttack = soldier.attack();
+        assertEquals(this.strength, resultAttack);
+    }
+
+
+    @Test
+    public void attack_should_return_the_strength_property_of_the_Soldier() {
+        Soldier soldier = new Soldier(health, strength);
+        int resultAttack = soldier.attack();
+        assertEquals(150, resultAttack);
+    }
+
+    @Test
+    public void damage_should_receive_1_arguments() {
+        Soldier soldier = new Soldier(health, strength);
+        soldier.receiveDamage(20);
+        assertEquals(280, soldier.health);
+    }
+}
